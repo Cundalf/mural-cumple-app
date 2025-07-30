fc5d2773db79c68cfbd70491991492d94107e7ea
Agustin Cundari <acundari95@gmail.com>
2025-07-29 23:19:00 -0300

    Actualiza copyright

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,9 +5,8 @@ import './globals.css'
 import { Toaster } from '@/components/ui/toaster'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.dev',
+  title: 'Mural Cumple App',
+  description: 'Created by Cundalf with v0, Next.js, Tailwind CSS and Cursor AI',
 }
 
 export default function RootLayout({
